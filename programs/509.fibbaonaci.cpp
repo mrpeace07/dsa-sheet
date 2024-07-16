e6fddf7f2835f228049509602deef652c7aae3e8
Nishanth S <140835316+mrpeace07@users.noreply.github.com>
2024-07-16 12:01:59 +0530

    Update 509.fibbaonaci.cpp

--- programs/509.fibbaonaci.cpp
+++ programs/509.fibbaonaci.cpp
@@ -1,18 +1,33 @@
+// class Solution {
+// public:
+//     int fib(int n) {
+//         // if( n == 0) return 0;
+//         // if( n == 1) return 1;
+
+//         // return fib(n-1) + fib(n-2);
+
+// vector<int> dp(n+1);
+// if ( n <= 1) return n;
+// dp[0] = 0;
+// dp[1] = 1;
+// for(int i=2 ; i<dp.size(); i++){
+//      dp[i]=  dp[i-1] + dp[i-2];
+// }
+// return dp[n];
+//     }
+// };
+
 class Solution {
 public:
     int fib(int n) {
-        // if( n == 0) return 0;
-        // if( n == 1) return 1;
-
-        // return fib(n-1) + fib(n-2);
-
-vector<int> dp(n+1);
-if ( n <= 1) return n;
-dp[0] = 0;
-dp[1] = 1;
-for(int i=2 ; i<dp.size(); i++){
-     dp[i]=  dp[i-1] + dp[i-2];
-}
-return dp[n];
+        if (n <= 1) return n;
+        
+        int prev1 = 1, prev2 = 0, current;
+        for (int i = 2; i <= n; ++i) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return current;
     }
 };
